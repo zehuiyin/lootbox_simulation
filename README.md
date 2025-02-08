79bc9a369239a11069ad8b73332629c6bc057396
zehuiyin <zehuiyin@gmail.com>
2025-02-08 13:09:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Loot Box Opening Simulation
+
+This Shiny app is built using Shinylive, which allows it to run entirely on the frontend without the need for a server. However, this also means that it may take some time to load in the web browser. 
+
+A common loot box scheme in games involves collecting a series of collectibles to earn the final reward. To acquire these collectibles, players must open cases that drop one collectible at a time, with the possibility of receiving duplicates. An exchange system often allows players to trade duplicate collectibles for new ones at a specified rate. The key question is: what is the probability of obtaining the final reward after opening a certain number of boxes?
+
+The app simulates 1,000 runs to determine the probability of successfully collecting the final reward when opening loot boxes in games.
+
+To access the web app, please click on this [link](zehuiyin.github.io/lootbox_simulation/).
---
+++ app/app.R
@@ -0,0 +1,98 @@
+library(shiny)
+library(bslib)
+library(ggplot2)
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "lumen"),
+  titlePanel(
+    h1("Loot Box Opening Simulation", align = "center", id = "title")
+  ),
+  div(
+    fluidRow(
+      column(10,
+             align="center",
+             span("A common loot box scheme in games involves obtaining a collection of collectibles to earn the final reward. To acquire these collectibles, players must open cases that drop one collectible at a time, with the possibility of receiving duplicates. An exchange system is often available, allowing players to trade duplicate collectibles for new ones at a specified rate. The question then arises: what is the probability of obtaining the final reward after opening a certain number of boxes?")
+      ),
+      column(2,
+             align="center",
+             HTML("<span>Created by <a href='https://zehuiyin.github.io/' target='_blank'>Zehui Yin</a></span>")
+      ),
+      style = 'border-bottom: 1px solid'
+    ),
+    fluidRow(
+      column(12,
+             align="center",
+             HTML("&nbsp;"))
+    ),
+    fluidRow(
+      column(4,
+             align="center",
+             numericInput("collect", 
+                          "How Many Collectibles Are There In The Collection", 
+                          min = 1, 
+                          step = 1, 
+                          value = 60)
+      ),
+      column(4,
+             align="center",
+             numericInput("box_opened", 
+                          HTML("Total Number of Cases Opened <br> &nbsp;"), 
+                          min = 1, 
+                          step = 1,
+                          value = 80)
+      ),
+      column(4,
+             align="center",
+             numericInput("exrate", 
+                          "How Many Duplicates Required to Trade A New One", 
+                          min = 1, 
+                          step = 1,
+                          value = 2)
+      ),
+      style = 'border-bottom: 1px solid'
+    ),
+    fluidRow(
+      column(12,
+             align="center",
+             HTML("&nbsp;"))
+    ),
+    fluidRow(
+      column(12,
+             align="center",
+             span("The optimal strategy is to refrain from exchanging any duplicates until you have accumulated enough to complete the entire collection.")
+      ),
+    ),
+    fluidRow(
+      column(12,
+             align="center",
+             textOutput("prob")
+      )
+    ), style = "max-width: 1200px; margin: auto;")
+)
+
+server <- function(input, output, session) {
+  output$prob <- renderText({
+    req(input$collect)
+    req(input$box_opened)
+    req(input$exrate)
+    
+    sims <- replicate(1000, {
+      s <- sample(1:input$collect, 
+                  size = input$box_opened,
+                  replace = TRUE)
+      s_no_dup <- unique(s)
+      
+      dup <- length(s) - length(s_no_dup)
+      uniq <- length(s_no_dup)
+      
+      dup >= (input$collect - uniq) * input$exrate
+    }, simplify = TRUE)
+    
+    res <- sum(sims)/length(sims)*100
+    
+    paste0("Based on a simulation of 1,000 runs, the probability of obtaining the final reward is ", 
+           res, "%")
+  })
+}
+
+shinyApp(ui, server)
